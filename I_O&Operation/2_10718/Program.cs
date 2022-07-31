5db7471df6968af102ae52454bc0e9272306b8fc
EoNjesajo <dnjsrua59@gmail.com>
2022-07-31 20:06:58 +0900

    22-07-31

---
+++ I_O&Operation/2_10718/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_10718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한육군 대한육군")
+            Console.WriteLine("강한육군 대한육군")
+            // string[] text = {"강한육군 대한육군", "강한육군 대한육군"};
+            // foreach (string str in text) 
+            //     Console.WriteLine(str);
+
+        }
+    }
+}
