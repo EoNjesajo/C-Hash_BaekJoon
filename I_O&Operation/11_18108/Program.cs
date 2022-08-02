82a5f437d1d1f9175e32f21e11f787665258dc5d
EoNjesajo <dnjsrua59@gmail.com>
2022-08-02 22:24:14 +0900

    22-08-02

---
+++ I_O&Operation/11_18108/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _11_18108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(input-543);
+        }
+    }
+}
