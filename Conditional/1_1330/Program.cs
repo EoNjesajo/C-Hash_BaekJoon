81301edadf31ca3a66c3a9b2d8289e02f92402a0
EoNjesajo <dnjsrua59@gmail.com>
2022-08-04 05:14:38 +0900

    22-08-04

---
+++ Conditional/1_1330/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_1330
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            if(input[0]<input[1])
+                Console.WriteLine("<");
+            else if(input[0]>input[1])
+                Console.WriteLine(">");
+            else
+                Console.WriteLine("==");
+        }
+    }
+}
